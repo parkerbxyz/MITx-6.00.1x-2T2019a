a7f0736cf9a51d5c6815a6fa20d67dc70187defa
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2019-09-14 17:16:49 -0400

    Create while.py

---
+++ Week 1 Exercises/while.py
@@ -0,0 +1,21 @@
+# exercise 1
+num = 2
+while num <= 10:
+    print(num)
+    num += 2
+print('Goodbye!')
+
+# exercise 2
+print('Hello!')
+number = 10
+while number >= 2:
+    print(number)
+    number -= 2
+
+# exercise 3
+start = 1
+result = 0
+while start <= end:
+    result += start
+    start += 1
+print(result)
