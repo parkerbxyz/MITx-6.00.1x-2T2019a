a6ab4db57b1dd6ad68d22c724c79724c95d64408
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2019-09-14 17:16:47 -0400

    Create vara_varb.py

---
+++ Week 1 Exercises/vara_varb.py
@@ -0,0 +1,8 @@
+if type(varA) is str or type(varB) is str:
+    print('string involved')
+elif varA == varB:
+    print('equal')
+elif varA > varB:
+    print('bigger')
+elif varA < varB:
+    print('smaller')
