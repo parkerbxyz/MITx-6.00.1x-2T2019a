098cd1aa3158ea71da4f659f16181cff48affa9f
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2019-09-15 23:01:57 -0400

    Create power_recur.py

---
+++ Week 2 Exercises/power_recur.py
@@ -0,0 +1,11 @@
+def recurPower(base, exp):
+    """
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    """
+    if exp == 0:
+        return 1
+    else:
+        return base * recurPower(base, exp-1)
