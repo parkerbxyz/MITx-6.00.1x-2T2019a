38fb7073b6fb1378dfa9309f39b9ed07d19cbd51
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2019-09-17 19:32:32 -0400

    Update problem_2.py

--- Problem Set 2/problem_2.py
+++ Problem Set 2/problem_2.py
@@ -1,17 +1,25 @@
+def remaining_balance(balance: float, annual_interest_rate: float,
+                      monthly_payment: float) -> float:
+    """Return the remaining balance after 12 months of monthly payments."""
+    monthly_interest_rate = annual_interest_rate / 12.0
+    for month in range(12):
+        unpaid_balance = balance - monthly_payment
+        balance = unpaid_balance + (monthly_interest_rate * unpaid_balance)
+    return round(balance, 2)
+
+
 def lowest_payment(balance: float, annual_interest_rate: float) -> float:
     """Return the minimum fixed monthly payment that is a multiple
     of $10 needed to pay off a credit card balance within 12 months.
     """
+    starting_balance = balance
     minimum_payment = 10
-    original_balance = balance
-    monthly_interest_rate = annual_interest_rate / 12.0
     while balance > 0:
-        for month in range(12):
-            unpaid_balance = balance - minimum_payment
-            balance = unpaid_balance + (monthly_interest_rate * unpaid_balance)
+        balance = remaining_balance(
+            balance, annual_interest_rate, minimum_payment)
         if balance > 0:
             minimum_payment += 10
-            balance = original_balance
+            balance = starting_balance
     return round(minimum_payment, 2)
 
 
